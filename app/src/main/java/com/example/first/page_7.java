a186d208857494c427a9687006827f7e0a0d2e6e
zain <i191675@nu.edu.pk>
2022-09-18 10:35:21 -0700

    Musify

---
+++ app/src/main/java/com/example/first/page_7.java
@@ -0,0 +1,14 @@
+package com.example.first;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class page_7 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_page7);
+    }
+}
